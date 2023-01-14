aa253b3d626313fd7de9b71c0fb0265b0e96537a
lg.cavalheiro <lg.cavalheiro@hotmail.com>
2023-01-14 03:44:41 -0300

    fix: husky commit hooks

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { createApp } from "./app/app";
 
 const app = createApp();
 
-/* tslintaa:disable-next-line: no-console */
+/* tslint:disable-next-line: no-console */
 app.listen(3000, () => console.log("App started"));
